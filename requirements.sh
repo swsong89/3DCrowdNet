a15ff80cff25ff5976117fd56d1813068b86a379
shaowei.song <571648942@qq.com>
2022-09-08 18:04:06 +0800

    first commit

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+pip install opencv-python
+pip install torchgeometry
+pip install transforms3d
+pip install pycocotools
+pip install chumpy
+pip install pyrender
+pip install trimesh
+pip install human-body-prior
+pip install pyyaml==5.4.1
+pip install easydict
+pip install tqdm
